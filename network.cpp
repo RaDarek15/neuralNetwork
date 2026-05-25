6bdf796e7506a83622c0361035d870a6895745eb
RaDarek15 <141033924+RaDarek15@users.noreply.github.com>
2026-05-25 22:55:58 +0200

    optimized a bunch and implemented the testing dataset

--- network.cpp
+++ network.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <fstream>
 #include <random>
+#include <algorithm>
 #include "json.hpp"
 #include "mnist.hpp"
 
@@ -29,7 +30,7 @@ public:
     std::vector<std::vector<double>> weights;
     std::vector<double> biases;
     layerCache forwardPass(
-        std::vector<double> inputy)
+        const std::vector<double>& inputy)
     {
         layerCache cache;
 
@@ -51,7 +52,7 @@ public:
     }
 
     Warstwa(int neuronCount, int inputCount);
-    Warstwa(std::vector<std::vector<double>> weights, std::vector<double> biases);
+    Warstwa(const std::vector<std::vector<double>>& weights, const std::vector<double>& biases);
 };
 Warstwa::Warstwa(int neuronCount, int inputCount)
 {
@@ -73,8 +74,8 @@ Warstwa::Warstwa(int neuronCount, int inputCount)
         biases[i] = dist(mt);
     }
 }
-Warstwa::Warstwa(std::vector<std::vector<double>> weights, std::vector<double> biases) : biases(biases), weights(weights) {};
-std::vector<double> error(std::vector<double> preActivation, std::vector<double> outputs, std::vector<double> expectedValues)
+Warstwa::Warstwa(const std::vector<std::vector<double>>& weights, const std::vector<double>& biases) : biases(biases), weights(weights) {};
+std::vector<double> error(const std::vector<double>& preActivation, const std::vector<double>& outputs, const std::vector<double>& expectedValues)
 {
     std::vector<double> error;
     for (int i = 0; i < preActivation.size(); i++)
@@ -83,7 +84,7 @@ std::vector<double> error(std::vector<double> preActivation, std::vector<double>
     }
     return error;
 }
-std::vector<double> propagateError(std::vector<double> error, Warstwa &layer, std::vector<double> preActivation)
+std::vector<double> propagateError(const std::vector<double>& error, const Warstwa &layer, const std::vector<double>& preActivation)
 {
     std::vector<double> out(layer.weights[0].size());
 
@@ -103,7 +104,7 @@ private:
 public:
     std::vector<Warstwa> layers;
 
-    std::vector<layerCache> forwardPass(std::vector<double> inputs)
+    std::vector<layerCache> forwardPass(const std::vector<double>& inputs)
     {
         std::vector<layerCache> caches;
         std::vector<double> current = inputs;
@@ -116,7 +117,7 @@ public:
         }
         return caches;
     }
-    double srStrata(std::vector<std::vector<double>> wynikSieci, std::vector<std::vector<double>> expectedValues)
+    double srStrata(const std::vector<std::vector<double>>& wynikSieci, const std::vector<std::vector<double>>& expectedValues)
     {
         double srStrata = 0.0;
         int i = 0;
@@ -130,7 +131,7 @@ public:
         return srStrata / expectedValues.size();
     }
 
-    std::vector<std::vector<std::vector<double>>> weightBackprop(std::vector<double> error, std::vector<layerCache> cache, std::vector<double> inputs)
+    std::vector<std::vector<std::vector<double>>> weightBackprop(std::vector<double> error, const std::vector<layerCache>& cache, const std::vector<double>& inputs)
     {
         std::vector<std::vector<std::vector<double>>> output(layers.size());
         for (int i = 0; i < layers.size(); i++)
@@ -165,7 +166,7 @@ public:
         }
         return output;
     }
-    std::vector<std::vector<double>> biasBackprop(std::vector<double> error, std::vector<layerCache> cache, std::vector<double> inputs)
+    std::vector<std::vector<double>> biasBackprop(std::vector<double> error, const std::vector<layerCache>& cache, const std::vector<double>& inputs)
     {
         std::vector<std::vector<double>> output(layers.size());
         for (int i = 0; i < layers.size(); i++)
@@ -185,7 +186,7 @@ public:
         }
         return output;
     }
-    void update(std::vector<std::vector<std::vector<double>>> weightNumGrad, std::vector<std::vector<double>> biasNumGrad, double learningRate)
+    void update(const std::vector<std::vector<std::vector<double>>>& weightNumGrad, const std::vector<std::vector<double>>& biasNumGrad, double learningRate)
     {
         for (int i = 0; i < layers.size(); i++)
         {
@@ -199,7 +200,7 @@ public:
             }
         }
     }
-    void train(std::vector<std::vector<double>> trainingData, std::vector<std::vector<double>> expectedValues, double learningRate, int iterations)
+    void train(const std::vector<std::vector<double>>& trainingData, const std::vector<std::vector<double>>& expectedValues, double learningRate, int iterations)
     {
         std::vector<layerCache> cache;
         for (int i = 0; i < iterations; i++)
@@ -219,7 +220,7 @@ public:
         }
     }
 
-    void save(std::string fileName, std::vector<std::vector<double>> expectedValues, std::vector<std::vector<double>> alloutputs)
+    void save(const std::string& fileName, const std::vector<std::vector<double>>& expectedValues, const std::vector<std::vector<double>>& alloutputs)
     {
         double ogStrata = srStrata(alloutputs, expectedValues);
         json j;
@@ -233,7 +234,7 @@ public:
         file << j.dump(4);
         file.close();
     }
-    void load(std::string fileName)
+    void load(const std::string& fileName)
     {
         json j = json::array();
         std::ifstream file(fileName);
@@ -246,13 +247,31 @@ public:
 
         file.close();
     }
-
-    siec(std::vector<Warstwa> layers);
+    double accuracy(const std::vector<std::vector<double>>& wynikSieci, const std::vector<std::vector<double>>& expectedValues)
+    {
+        int counter = 0;
+        int i;
+        for (i = 0; i < expectedValues.size(); i++)
+        {
+            int predicted = std::max_element(wynikSieci[i].begin(), wynikSieci[i].end()) - wynikSieci[i].begin();
+            int expceted = std::max_element(expectedValues[i].begin(), expectedValues[i].end()) - expectedValues[i].begin();
+            if (expceted == predicted) 
+            {
+                counter++;
+            }
+            
+        }
+        return (double)counter/i*100;
+        
+    }
+    siec(const std::vector<Warstwa>& layers);
 };
-siec::siec(std::vector<Warstwa> layers) : layers(layers) {}
+siec::siec(const std::vector<Warstwa>& layers) : layers(layers) {}
 
 int main()
 {
+    char yesNo;
+    std::vector<std::vector<double>> wyniki;
     std::ifstream f("model.json");
     data data = readMnist("train-images.idx3-ubyte", "train-labels.idx1-ubyte");
     std::vector<std::vector<double>> trainingData = data.images, expectedValues = data.labels;
@@ -260,10 +279,19 @@ int main()
     if (f)
     {
         mnist.load("model.json");
+        std::cout << "Model loaded! \n";
     }
 
-    else
+    std::cout << "do you want to train more? (y/n)\n";
+    std::cin >> yesNo;
+    if (yesNo == 'y')
     {
         mnist.train(trainingData, expectedValues, 0.01, 3);
     }
+    data = readMnist("t10k-images.idx3-ubyte", "t10k-labels.idx1-ubyte");
+    for (int i = 0; i < data.images.size() ; i++)
+    {
+        wyniki.push_back(mnist.forwardPass(data.images[i]).back().postActivation);
+    }
+    std::cout << "Accuracy: " << mnist.accuracy(wyniki, data.labels) << "%\n";
 }
